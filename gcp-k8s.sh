f9ab696a69f07e33d969fb3591665889bce4dfe5
braveantony <90317293+braveantony@users.noreply.github.com>
2024-04-13 15:34:12 +0800

    Create gcp-k8s.sh

---
+++ gcp-k8s.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd
+kubectl cp ~/wulin-gcp/bin/ kube-system/kube-kadm:/home/bigred/wulin -c kadm
+echo "copy wulin/bin ok"
+kubectl cp ~/wulin-gcp/images/ kube-system/kube-kadm:/home/bigred/wulin/ -c kadm
+echo "copy wulin-gcp/images ok"
+kubectl cp ~/wulin/wkload/ kube-system/kube-kadm:/home/bigred/wulin/ -c kadm
+echo "copy wulin-gcp/wkload ok"' 
